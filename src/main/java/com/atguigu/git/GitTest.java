e6ee11fe42616825884d8f8c888730f989476941
tangheng-ops <84770056+tangheng-ops@users.noreply.github.com>
2022-02-13 18:15:39 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hello git4");
         System.out.println("hello git4");
+        System.out.println("pull test");
     }
 
 }
